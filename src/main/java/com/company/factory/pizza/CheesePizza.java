622abe2f11cfc77f710cdd69dfc0e9d635c08f6e
oskoblya <oleg.skoblya@meta.ua>
2015-06-13 06:19:33 +0300

    added pizza!! :)

---
+++ src/main/java/com/company/factory/pizza/CheesePizza.java
@@ -0,0 +1,22 @@
+package com.company.factory.pizza;
+
+/**
+ * Created by à ä ì è í on 13.06.2015.
+ */
+public class CheesePizza implements Pizza {
+    public void prepare() {
+
+    }
+
+    public void bake() {
+
+    }
+
+    public void cut() {
+
+    }
+
+    public void box() {
+
+    }
+}
---
+++ src/main/java/com/company/factory/pizza/Pizza.java
@@ -0,0 +1,11 @@
+package com.company.factory.pizza;
+
+/**
+ * Created by à ä ì è í on 13.06.2015.
+ */
+public interface Pizza {
+    void prepare();
+    void bake();
+    void cut();
+    void box();
+}
