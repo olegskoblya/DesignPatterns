de8dad421f6fa52e290835610c4e07e52a8ae982
oleg <oleg.skoblya@meta.ua>
2015-06-14 13:53:53 +0300

    some addition to the package "factory"

--- src/main/java/com/company/factory/pizza/Pizza.java
+++ src/main/java/com/company/factory/pizza/Pizza.java
@@ -1,11 +1,55 @@
 package com.company.factory.pizza;
 
+import java.util.ArrayList;
+
 /**
- * Created by à ä ì è í on 13.06.2015.
+ * Created by ï¿½ ï¿½ ï¿½ ï¿½ ï¿½ on 13.06.2015.
  */
-public interface Pizza {
-    void prepare();
-    void bake();
-    void cut();
-    void box();
+public abstract class Pizza {
+    String name;
+    String dough;
+    String sauce;
+    ArrayList<String> toppings = new ArrayList();
+
+
+    public void prepare() {
+        System.out.println("Preparing " + name);
+        System.out.println("Tossing dough...");
+        System.out.println("Adding sauce...");
+        System.out.println("Adding toppings: ");
+        for (String topping: toppings) {
+            System.out.println("   " + topping);
+        }
+    };
+    public void bake() {
+        System.out.println("Bake for 25 minutes at 350");
+    };
+
+    public void cut() {
+        System.out.println("Cutting the pizza into diagonal slices");
+    };
+    public void box() {
+        System.out.println("Place pizza in official PizzaStore box");
+    };
+
+    public String getName() {
+        return name;
+    }
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDough(String dough) {
+        this.dough = dough;
+    }
+
+    public void setSauce(String sauce) {
+        this.sauce = sauce;
+    }
+
+    public void setToppings(ArrayList<String> toppings) {
+        this.toppings = toppings;
+    }
 }
---
+++ src/main/java/com/company/factory/pizza/PizzaStore.java
@@ -0,0 +1,7 @@
+package com.company.factory.pizza;
+
+/**
+ * Created by oleg on 14.06.15.
+ */
+public class PizzaStore {
+}
