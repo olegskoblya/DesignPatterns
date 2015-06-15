4bd7a019583c03bccbfea9acde79b4469bc15389
oskoblya <oleg.skoblya@meta.ua>
2015-06-15 17:49:46 +0300

    somesome

--- src/main/java/com/company/factory/pizza/pizza2/chicago/pizzas/CheesePizza.java
+++ src/main/java/com/company/factory/pizza/pizza2/chicago/pizzas/CheesePizza.java
@@ -1,12 +1,22 @@
 package com.company.factory.pizza.pizza2.chicago.pizzas;
 
 import com.company.factory.pizza.pizza2.Pizza;
+import com.company.factory.pizza.pizza2.PizzaIngridientFactory;
 
 /**
  * Created by oskoblya on 15.06.2015.
  */
 public class CheesePizza extends Pizza {
-    public void prepare() {
+    PizzaIngridientFactory ingridientFactory;
 
+    public CheesePizza(PizzaIngridientFactory ingridientFactory) {
+        this.ingridientFactory = ingridientFactory;
+    }
+
+    public void prepare() {
+        System.out.println("Preparing " + name);
+        dough = ingridientFactory.createDough();
+        sauce = ingridientFactory.createSauce();
+        cheese = ingridientFactory.createCheese();
     }
 }
