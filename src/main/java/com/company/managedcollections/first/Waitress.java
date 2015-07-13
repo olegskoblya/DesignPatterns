862dad6970f26cab82f50d24b1b39e346a7668bb
oskoblya <oleg.skoblya@meta.ua>
2015-07-13 10:55:58 +0300

    somesome

--- src/main/java/com/company/managedcollections/first/Waitress.java
+++ src/main/java/com/company/managedcollections/first/Waitress.java
@@ -9,26 +9,31 @@ import java.util.Iterator;
 public class Waitress {
     Menu dinerMenu;
     Menu pancakeHouseMenu;
+    Menu cafeMenu;
 
-    public Waitress(Menu dinerMenu, Menu pancakeHouseMenu) {
+    public Waitress(Menu dinerMenu, Menu pancakeHouseMenu, Menu cafeMenu) {
         this.dinerMenu = dinerMenu;
         this.pancakeHouseMenu = pancakeHouseMenu;
+        this.cafeMenu = cafeMenu;
     }
 
     public void printMenu() {
         Iterator pancakeIterator = pancakeHouseMenu.createIterator();
         Iterator dinerIterator = dinerMenu.createIterator();
+        Iterator cafeIterator = cafeMenu.createIterator();
         System.out.println("MENU\n----------\nBREAKFAST");
         printMenu(pancakeIterator);
         System.out.println("\nLUNCH");
         printMenu(dinerIterator);
+        System.out.println("\nDINNER");
+        printMenu(cafeIterator);
     }
 
     private void printMenu(Iterator iterator) {
         while (iterator.hasNext()) {
             MenuItem menuItem = (MenuItem) iterator.next();
-            System.out.println(menuItem.getName() + ", ");
-            System.out.println(menuItem.getPrice() + " -- ");
+            System.out.print(menuItem.getName() + ", ");
+            System.out.print(menuItem.getPrice() + " -- ");
             System.out.println(menuItem.getDescription());
         }
     }
